8b7240aef091bac12f062dc2b947f6a53b245e4d
annamhalicka <annamhalicka@gmail.com>
2021-02-23 17:26:25 +0100

    Dodałam main.fxml z wyglądem, dodałam css i grafikę potrzebną do wyglądu. Są też już zdefiniowane id do buttonów i OnAction.

--- src/main/java/controllers/Main.java
+++ src/main/java/controllers/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/views/main.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1025, 625));
         primaryStage.show();
     }
 
